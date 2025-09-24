4edcf20e44f1595b05ab89edc4b76856821623d0
cmdr <cmdr@commander23.net>
2025-09-24 10:38:53 +0200

    updatet bootstrap

--- bootstrap_container.sh
+++ bootstrap_container.sh
@@ -1,12 +1,14 @@
 apt update && apt upgrade 
 
-sudo apt install git sudo btop tmux
+sudo apt install git sudo btop tmux stow trash
 
 useradd -m -s /bin/bash cmdr
 printf '\n\n\nenter password for cmdr\n'
 passwd cmdr
 adduser cmdr sudo
 
+git clone https://github.com/Commander-23/dotfiles.git /home/cmdr
+git clone https://github.com/Commander-23/runs.git /home/cmdr
 
-
-
+chown cmdr:cmdr /home/cmdr/dotfiles
+chown cmdr:cmdr /home/cmdr/runs 
