3ffcdb3f630032246a25c09e3025664254b071a1
cmdr <cmdr@commander23.net>
2025-09-24 10:46:13 +0200

    updated scripts

--- bootstrap_container.sh
+++ bootstrap_container.sh
@@ -6,9 +6,3 @@ useradd -m -s /bin/bash cmdr
 printf '\n\n\nenter password for cmdr\n'
 passwd cmdr
 adduser cmdr sudo
-
-git clone https://github.com/Commander-23/dotfiles.git /home/cmdr
-git clone https://github.com/Commander-23/runs.git /home/cmdr
-
-chown cmdr:cmdr /home/cmdr/dotfiles
-chown cmdr:cmdr /home/cmdr/runs 
---
+++ configure_dotfiles.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+git clone https://github.com/Commander-23/runs.git
+git clone https://github.com/Commander-23/dotfiles.git
