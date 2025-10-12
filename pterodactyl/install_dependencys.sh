a980bc1dadaa45255a063595b994d1ffffbbfd88
cmdr <cmdr@commander23.net>
2025-10-12 18:48:39 +0200

    nginx conf added - script change

--- pterodactyl/install_dependencys.sh
+++ pterodactyl/install_dependencys.sh
@@ -13,3 +13,7 @@ echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://pack
 # Update repositories list and install Dependencies
 sudo apt update
 sudo apt -y install php8.3 php8.3-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
