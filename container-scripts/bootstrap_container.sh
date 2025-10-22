7d465885f106294a8b9aefd28d70bdedf73a5c41
cmdr <cmdr@commander23.net>
2025-10-23 01:06:44 +0200

    updated to include necessaries only

--- container-scripts/bootstrap_container.sh
+++ container-scripts/bootstrap_container.sh
@@ -1,6 +1,6 @@
 apt update && apt upgrade 
 
-sudo apt install git sudo btop tmux stow trash
+apt install git sudo btop tmux
 
 useradd -m -s /bin/bash cmdr
 printf '\n\n\nenter password for cmdr\n'
